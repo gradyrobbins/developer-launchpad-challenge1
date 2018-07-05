9a724830c4787a8cd61ec7a132581d5b1f06ac23
Grady Robbins <38964004+gradyrobbins@users.noreply.github.com>
2018-07-04 22:08:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,3 +18,11 @@ Happy Numbers Below 100:
 console.log("hello world");
 
 var userInput = prompt("choose a number between 1 and 100", "xxx");
+
+let arrayPrimeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+let arrayHappyNumbers = [1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100];
+
+
+
+
