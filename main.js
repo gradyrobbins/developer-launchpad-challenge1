3c6ba59a67a4309cf317b7cdf04dc3104ad81b39
Grady Robbins <38964004+gradyrobbins@users.noreply.github.com>
2018-07-04 21:38:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-lorem ipsum javascript
+/*lorem ipsum javascript
+
+Level 1 In the first level you will start by producing a list of the happy primes below 100 from a list of primes 
+and a list of happy numbers. Then take a user input and determine if that number is a happy prime or not.
+
+Prime Numbers Below 100:
+
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+Happy Numbers Below 100:
+
+[1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100]
+
+
+*/
+
+
+console.log("hello world");
+
+var userInput = prompt("choose a number between 1 and 100", "xxx");
