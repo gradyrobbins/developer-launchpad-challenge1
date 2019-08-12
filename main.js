e1460d5f6d55f42ca59533e96e5e54d04fbe8c61
Grady Robbins <gradyrobbins13@gmail.com>
2019-08-12 11:26:11 -0500

    file structure improvments and import images

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ happy.innerHTML += happy100;
 
 let prime = document.getElementById("prime")
 let prime100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-// below array via google search.  copy/pasta
+// prime500 array via google search.  copy/pasta
 let prime500 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499
 ];
 prime.innerHTML += prime100;
@@ -27,8 +27,10 @@ function getMatch(a, b) {
     }
     return matches;
 }
+
 let x = getMatch(prime100, happy100)
 happyprime100.push(x);
+
 // happyprime.innerHTML += happyprime100
 
 
